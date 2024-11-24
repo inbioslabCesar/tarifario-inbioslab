991b3fb580cf6f159a1c2a29e84e670a553bd4bc
inbioslabCesar <97005412+inbioslabCesar@users.noreply.github.com>
2024-11-24 14:32:43 -0500

    Adicionando precio publico en ProducRow

--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import style from "./ProductModal.module.css";
 
 const ProductModal = ({
--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -2,16 +2,23 @@ import { useState } from "react";
 import ProductModal from "./ProductModal";
 import style from "./ProductRow.module.css";
 
-const ProductRow = ({ name, price2, time, info, tube, method }) => {
+const ProductRow = ({ name, price1, price2, time, info, tube, method }) => {
   const [showModal, setShowModal] = useState(false);
+  const [active, setActive] = useState(false);
+  console.log(active);
   return (
     <div className={style.product}>
       <div></div>
       <div className={style.name}>
         <span>{name}</span>
       </div>
+      {active && (
+        <div className={style.publico}>
+          <span>{price1}</span>
+        </div>
+      )}
       <div className={style.price}>
-        <span>S/ {price2}.00</span>
+        <span onClick={() => setActive(!active)}>S/ {price2}.00</span>
       </div>
       <div className={style.time}>
         <span>{time}</span>
